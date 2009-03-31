04641dd975c367fb5d5a08c9ce25ea6d3a1d1418
vincent.benayoun <vincent.benayoun@afd29ea4-0d89-11de-b025-799c3e7cca98>
2009-03-31 14:43:28 +0000

    Modifications : -les pages correspondant à une file de discussion sont maintenant entièrement analysées et extraites (servlet : ExtractTopics) -la servlet SubDiscussionChecker permet maintenant de vérifier si la page à extraire correspond à la liste des différentes files de discussion, la méthode ExtractTopics est alors appelée sur tous les files en question.

---
+++ ProjetAppliWeb/src/servlet/ExtractTopics.java
@@ -0,0 +1,266 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.htmlparser.Node;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.filters.HasParentFilter;
+import org.htmlparser.filters.HasSiblingFilter;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.nodes.TagNode;
+import org.htmlparser.nodes.TextNode;
+import org.htmlparser.tags.LinkTag;
+import org.htmlparser.util.NodeIterator;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
+import utils.Post;
+
+/**
+ * Servlet implementation class Discussion
+ */
+public class ExtractTopics extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ExtractTopics() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String adresse = request.getParameter("adresse");
+		extractalltag(out, adresse);
+	}
+
+	public void extractalltag(PrintWriter out, String adresse) {
+		Parser parser = null;
+		AndFilter sujetFiltre = new AndFilter(new TagNameFilter("div"),new HasAttributeFilter("class", "subject"));
+		AndFilter idFrere = new AndFilter(new TagNameFilter("table"),
+				new HasAttributeFilter("class", "forumpost"));
+		AndFilter idFiltre = new AndFilter(new TagNameFilter("a"),
+				new HasSiblingFilter(idFrere));
+		AndFilter messageFiltre = new AndFilter(new TagNameFilter("td"),
+				new HasAttributeFilter("class", "content"));
+		AndFilter divAuteur = new AndFilter(new TagNameFilter("div"),
+				new HasAttributeFilter("class", "author"));
+		AndFilter auteurFiltre = new AndFilter(new TagNameFilter("a"),
+				new HasParentFilter(divAuteur));
+		AndFilter divCommands = new AndFilter(new TagNameFilter("div"),
+				new HasAttributeFilter("class", "commands"));
+		AndFilter parentFiltre = new AndFilter(new TagNameFilter("a"),
+				new HasParentFilter(divCommands));
+
+		try {
+			parser = new Parser(adresse);
+
+			// parser.setResource("file:///home/max06/localhost/moodle/mod/forum/discuss.php%3Fd=2");
+		} catch (ParserException e1) {
+			e1.printStackTrace();
+		}
+		try {
+			List<Post> res = new ArrayList<Post>();
+			NodeList list = parser.parse(sujetFiltre);
+			for (int i = 0; i < list.size(); i++) {
+				Node tmp = list.elementAt(i);
+				String sujet = extractTextNode(tmp);
+				parser.reset();
+
+				list = parser.parse(messageFiltre);
+				Node tmp2 = list.elementAt(i);
+				String message = extractNodeToHtml(tmp2);
+				parser.reset();
+
+				list = parser.parse(auteurFiltre);
+				Node tmp3 = list.elementAt(i);
+				String auteur = extractLinkTagText(tmp3);
+				parser.reset();
+
+				list = parser.parse(divAuteur);
+				Node tmp4 = list.elementAt(i);
+				String date = extractDate(tmp4);
+				parser.reset();
+
+				list = parser.parse(idFiltre);
+				Node tmp5 = list.elementAt(i);
+				String id = extractLinkTagAttribute(tmp5, "id");
+				parser.reset();
+				
+				String parent = null;
+//				if (i != 0) {
+//					list = parser.parse(parentFiltre);
+//					Node tmp6 = list.elementAt(i);
+//					String linkParent = extractLinkParent(tmp6, out);
+////				parent = extractIdParent(linkParent);
+//					parser.reset();
+//				}
+
+				list = parser.parse(sujetFiltre);
+				Post p = new Post(id, sujet, auteur, message, date, parent);
+				res.add(p);
+			}
+			 showAllPost(res, out);
+		} catch (ParserException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String extractLinkParent(Node node, PrintWriter out)
+			throws ParserException {
+		if (node instanceof TagNode) {
+			TagNode tag = (TagNode) node;
+			if (tag instanceof LinkTag) {
+				LinkTag link = (LinkTag) tag;
+				NodeList nl = link.getChildren();
+				if (nl != null) {
+					for (NodeIterator i = nl.elements(); i.hasMoreNodes();) {
+						Node tmp = i.nextNode();
+						if (tmp.getText().equals("Show parent")) {
+							String res = link.getLink();
+							return res;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public String extractLinkTagAttribute(Node node, String attribute)
+			throws ParserException {
+		if (node instanceof TagNode) {
+
+			TagNode tag = (TagNode) node;
+			String tagName = tag.getTagName();
+			if (tag instanceof LinkTag) {
+				LinkTag link = (LinkTag) tag;
+				return link.getAttribute("id");
+			}
+			return null;
+		}
+		return null;
+	}
+
+	public String extractLinkTagText(Node node) throws ParserException {
+		if (node instanceof TagNode) {
+
+			TagNode tag = (TagNode) node;
+			String tagName = tag.getTagName();
+			if (tag instanceof LinkTag) {
+				LinkTag link = (LinkTag) tag;
+				return link.getLinkText();
+			}
+			return null;
+		}
+		return null;
+	}
+
+	public String extractNodeToHtml(Node node) throws ParserException {
+		if (node instanceof TextNode) {
+
+			TextNode text = (TextNode) node;
+			String el = text.toHtml();
+			return el;
+		} else if (node instanceof TagNode) {
+
+			TagNode tag = (TagNode) node;
+			if (tag.getAttribute("class") != null
+					&& tag.getAttribute("class").equals("commands")) {
+				return null;
+			} else {
+				NodeList nl = tag.getChildren();
+				String res = "";
+				if (null != nl)
+					for (NodeIterator i = nl.elements(); i.hasMoreNodes();) {
+						Node n = i.nextNode();
+						if (n instanceof TagNode) {
+							TagNode tmp = (TagNode) n;
+							if (tmp.getAttribute("class") != null
+									&& tmp.getAttribute("class").equals(
+											"commands")) {
+								break;
+							}
+						}
+						res += n.toHtml();
+					}
+				return res;
+			}
+		}
+		return null;
+	}
+
+	public String extractTextNode(Node node) throws ParserException {
+		if (node instanceof TextNode) {
+
+			TextNode text = (TextNode) node;
+			return text.getText();
+		} else if (node instanceof TagNode) {
+
+			TagNode tag = (TagNode) node;
+			NodeList nl = tag.getChildren();
+			if (null != nl)
+				for (NodeIterator i = nl.elements(); i.hasMoreNodes();) {
+					if (!(tag.getAttribute("class")).equals("commands")) {
+						return extractTextNode(i.nextNode());
+					}
+				}
+
+		}
+		return null;
+	}
+
+	public void showAllPost(List l, PrintWriter out) {
+		for (Post p : (ArrayList<Post>) l) {
+			out.println("Id: " + p.getId());
+			out.println("Auteur: " + p.getAuteur());
+			out.println("Date: " + p.getDate());
+			out.println("topic: " + p.getSujet());
+			out.println("message: " + p.getMessage());
+			out.println("parent: " + p.getParent());
+			out.println();
+		}
+	}
+
+	public String extractDate(Node auteur) {
+		String date = null;
+		String ligne = auteur.toPlainTextString();
+		String[] res = ligne.split("-");
+		date = res[1];
+		return date;
+	}
+
+	public String extractIdParent(String linkParent) {
+		String idParent = null;
+		String[] res = linkParent.split("#");
+		idParent = res[1];
+		return idParent;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ ProjetAppliWeb/src/servlet/SubDiscussionChecker.java
@@ -0,0 +1,74 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.htmlparser.Parser;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.nodes.TagNode;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
+/**
+ * Servlet implementation class SubDiscussionChecker
+ */
+public class SubDiscussionChecker extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private final String pathToLocalForumFiles = "/home/vincent/wget";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SubDiscussionChecker() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String adresse = request.getParameter("adresse");
+		try {
+			Parser parser = new Parser(adresse);
+			AndFilter discussionsFilter = new AndFilter(new TagNameFilter("table"), new HasAttributeFilter("class", "forumheaderlist"));
+			NodeList list = parser.parse(discussionsFilter);
+			if(list.size() > 0) {
+				getAndExtractSubDiscussions(adresse, response.getOutputStream());
+			}
+		} catch (ParserException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	private void getAndExtractSubDiscussions(String adresse, ServletOutputStream out) throws ParserException, IOException {
+		Parser parser = new Parser(adresse);
+		AndFilter topicFilter = new AndFilter(new TagNameFilter("td"), new HasAttributeFilter("class", "topic starter"));
+		NodeList topicsList = parser.parse(topicFilter);
+		if(topicsList.size() > 0) {
+			for (int i = 0; i < topicsList.size(); i++) {
+				TagNode hrefChild = (TagNode) topicsList.elementAt(i).getChildren().elementAt(0);
+				String href = hrefChild.getAttribute("href");
+				href.replaceFirst("http://", "file://" + pathToLocalForumFiles + "/");
+				String formatedURL = java.net.URLEncoder.encode(href);
+				out.println(formatedURL);
+			}
+		}
+	}
+}
